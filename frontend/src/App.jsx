7b7b2e2b72a14cc373129be85ad91a2a18e9839e
Gustavo Souza <gustavoelia7@gmail.com>
2024-07-23 22:52:55 -0300

    initial files: frontend (setup React and Tailwind with Vite)

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './css/index.css';
+
+export default function App() {
+    return (
+        <div className="text-center">
+            <h1>Hello World</h1>
+        </div>
+    );
+}
